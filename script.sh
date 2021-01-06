c9272d0b649e9300889b72d3cc4852830843bd64
Primal Victor <47473604+xPrimalVictor7x@users.noreply.github.com>
2021-01-06 10:25:35 -0800

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This is a quick script to add the files/programs/folders/directories into the GitHub repo
-
-git add --all
-
-git commit -m "Initial commit"
-
-git push -u origin master
